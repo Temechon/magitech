3cff76ce873771dd821f1292cffd9d33bff07f86
Temechon <temechon@gmail.com>
2015-12-14 12:30:58 +0100

    update

--- src/js/dist/Cell.js
+++ src/js/dist/Cell.js
@@ -7,26 +7,28 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Cell = (function (_GameObject) {
-    _inherits(Cell, _GameObject);
+        _inherits(Cell, _GameObject);
 
-    function Cell(game) {
-        _classCallCheck(this, Cell);
+        function Cell(game) {
+                _classCallCheck(this, Cell);
 
-        var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Cell).call(this, game));
+                var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Cell).call(this, game));
 
-        _this.position = BABYLON.Vector3.Zero();
-        _this.size = 1;
+                _this.position = BABYLON.Vector3.Zero();
+                _this.size = 1;
 
-        _this.isVisible = true;
+                _this.isVisible = true;
 
-        // A cell is a squared plane
-        var vd = BABYLON.VertexData.CreateGround({
-            width: _this.size, height: _this.size, subdivisions: 1
-        });
-        vd.applyToMesh(_this, false);
-        return _this;
-    }
+                // A cell is a squared plane
+                var vd = BABYLON.VertexData.CreateGround({
+                        width: _this.size, height: _this.size, subdivisions: 1
+                });
+                vd.applyToMesh(_this, false);
 
-    return Cell;
+                _this.material;
+                return _this;
+        }
+
+        return Cell;
 })(GameObject);
 //# sourceMappingURL=Cell.js.map
